94a0ecc5a53f43e323d2a6c5bb43122ffa6968a7
varin_pimda <85553410+kumA0taku@users.noreply.github.com>
2022-01-08 22:42:24 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayStackTest
+{
+    public class ArrayStackTest
+    {
+        public static void Main(string[] args)
+        {
+            ArrayStack stack = new ArrayStack(10);
+
+            for(int i = 0; i < 10; i++)
+            {
+                string objToPush = "obj"+ i;
+                Console.WriteLine("pushing: {0}",objToPush);
+                stack.push(objToPush);
+            }
+            while(!stack.isEmpty())
+            {
+                string objPoped = (string)stack.pop();
+                Console.WriteLine("poping: {0}", objPoped);
+            }
+        }
+    }
+}
