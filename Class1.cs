ae486d8c13f3efae2076bfe629f87d8a0354965e
varin_pimda <85553410+kumA0taku@users.noreply.github.com>
2022-01-08 22:29:37 +0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTest
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MDB1");
+            MyDatabase mdb1 = new MyDatabase();
+            mdb1.ShowCustomer();
+            Console.WriteLine("MDB2");
+            MyDatabase mdb2 = new MyDatabase(101, "Thai", "RT 106");
+            mdb2.ShowCustomer();
+        }
+    }
+}
---
+++ MyDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTest
+{
+    public class MyDatabase
+    {
+        int customerID;
+        string strName;
+        string strAddress;
+
+        //method constructor 1
+        public MyDatabase()
+        {
+            customerID = -1;
+            strName = "unknow";
+            strAddress = "unknow";
+        }
+
+        //method constructor
+        public MyDatabase(int id, string name, string address)
+        {
+            customerID = id;
+            strName = name;
+            strAddress = address;
+        }
+        public void ShowCustomer()
+        {
+            Console.WriteLine("Customer ID is {0}", customerID);
+            Console.WriteLine("Customer name is {0}", strName);
+            Console.WriteLine("Customer address is {0}", strAddress);
+            Console.WriteLine();
+        }
+    }
+}
