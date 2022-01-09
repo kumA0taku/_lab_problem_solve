529391255ea2b3be5e54b9883cb663159dfd5e03
varin_pimda <85553410+kumA0taku@users.noreply.github.com>
2022-01-10 00:17:21 +0700

    Add files via upload

---
+++ ArrayQueueTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayQueueTest
+{
+    class ArrayQueueTest
+    {
+        public static void Main(string[] args)
+        {
+            ArrayQueue queue = new ArrayQueue(10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                string objToQueue = "obj " + i;
+                Console.WriteLine("enqueueing: {0}", objToQueue);
+                queue.enqueue(objToQueue);
+            }
+            Console.WriteLine();
+            while (!queue.isEmpty())
+            {
+                string objFromQueue = (string)queue.dequeue();
+                Console.WriteLine("dequeued: {0}", objFromQueue);
+            }
+        }
+    }
+}
