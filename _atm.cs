e56518d4f3a41c86bf2e8937103b6a8f0b2f2e5a
varin_pimda <85553410+kumA0taku@users.noreply.github.com>
2022-01-10 14:40:33 +0700

    Add files via upload

---
+++ _atm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTest
+{
+    public class AruATM
+    {
+        int amount = 0, deposit, withdraw;
+        int choice;
+
+        //method constructor 1
+        public AruATM()
+        {
+            while (true)
+            {
+                Console.WriteLine("WELCOME TO YES BANK ATM SERVICE\n");
+                Console.WriteLine("1. Current Balance\n");
+                Console.WriteLine("2. Withdraw \n");
+                Console.WriteLine("3. Deposit \n");
+                Console.WriteLine("4. Cancel \n");
+                Console.WriteLine("***************\n\n");
+                Console.WriteLine("ENTER YOUR CHOICE : ");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("\n YOUR CURRENT BALANCE IS : {0} ", amount);
+                        break;
+                    case 2:
+                        Console.WriteLine("\n ENTER THE WITHDRAW AMOUNT : ");
+                        withdraw = int.Parse(Console.ReadLine());
+                        if (withdraw % 100 != 0)
+                        {
+                            Console.WriteLine("\n PLEASE ENTER THE AMOUNT IN ABOVE 100");
+                        }
+                        else if (withdraw > (amount - 100))
+                        {
+                            Console.WriteLine("\n SORRY! INSUFFICENT BALANCE");
+                        }
+                        else
+                        {
+                            amount = amount - withdraw;
+                            Console.WriteLine("\n\n PLEASE COLLECT YOUR CASH");
+                            Console.WriteLine("\n CURRENT BALANCE IS Rs : {0}", amount);
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("\n ENTER THE DEPOSIT AMOUNT");
+                        deposit = int.Parse(Console.ReadLine());
+                        amount = amount + deposit;
+                        Console.WriteLine("YOUR AMOUNT HAS BEEN DEPOSITED SUCCESSFULLY..");
+                        Console.WriteLine("YOUR TOTAL BALANCE IS Rs : {0}", amount);
+                        break;
+                    case 4:
+                        Console.WriteLine("\n THANK YOU…");
+                        break;
+                }
+            }
+            Console.WriteLine("\n\n THANKS FOR USING YES ATM SERVICE");
+        }
+    }
+}
---
+++ _main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTest
+{
+    class _main 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your 4 Digit Pin ");
+            int _pin = int.Parse(Console.ReadLine());
+            AruATM _my_atm = new AruATM();
+        }
+    }
+}
