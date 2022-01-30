0ef6283d62364a38b85ef448652aa6096631ede7
varin_pimda <85553410+kumA0taku@users.noreply.github.com>
2022-01-30 15:25:02 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReverseString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void reverseBotton_Click(object sender, EventArgs e)
+        {
+            ArrayStack S = new ArrayStack(100);
+            string message = inputBox.Text;
+            
+            for(int i =0; i<message.Length; i++)
+            {
+                S.push(message[i]);
+            }
+
+            //create var data for store value
+            string data = "";
+            while(!S.isEmpty())
+            {
+                data=data+S.pop();  
+            }
+            outputBox.Text = data;  
+        }
+
+        private void clearBotton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text = "";
+            outputBox.Text = "";
+        }
+    }
+}
